45ee81280807150d31b0af183a42cd939a7e51d6
wopupa21 <77881102+wopupa21@users.noreply.github.com>
2023-06-02 22:55:34 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware')
 module.exports = (req, res) => {
   let target = ''
   if (req.url.startsWith('/backend')) {
-    target = 'https://buying999.sharepoint.com'
+    target = 'https://buying999.sharepoint.com/$1'
   }
   createProxyMiddleware({
     target,
